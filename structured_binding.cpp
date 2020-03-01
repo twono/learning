7069236c563c2858c3e91e8f70b59236522cd517
twono <twono.dev@gmail.com>
2020-03-01 00:51:17 +0000

    Update structured_binding.cpp

--- structured_binding.cpp
+++ structured_binding.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <array>
 #include <string>
-#include <tuple>
-#include <vector>
 using namespace std;
 
 class X
@@ -13,20 +11,23 @@ public:
 
     void print() { cout << a << " " << c << endl; }
 
-    template<int N> auto& get()
-    {
-        if constexpr (N == 0) return a;
-        else return c;
-    }
+    int getA() const { return a; }
+    string getC() const { return c; }
 
 private:
     int a = -13;
     string c = "hello";
 };
 
+template<int N> auto get(const X& x)
+{
+    if constexpr (N == 0) return x.getA();
+    else return x.getC();
+}
+
 namespace std
 {
-    template<> struct tuple_size<X> { static const int value = 2; };
+    template<> struct tuple_size<X> { static constexpr int value = 2; };
     template<> struct tuple_element<0, X>{ using type = int; };
     template<> struct tuple_element<1, X>{ using type = std::string; };
 }
