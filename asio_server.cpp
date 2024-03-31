0acc4a00e96f87a6d3226d78bed56514301328d7
twono <twono.dev@gmail.com>
2024-03-31 22:01:50 +0100

    asio_server

--- asio_server.cpp
+++ asio_server.cpp
@@ -5,50 +5,56 @@
 #include <thread>
 #include <memory>
 using namespace std;
+using namespace boost;
 using namespace std::chrono_literals;
-namespace io = boost::asio;
+
+class server
+{
+public:
+    server(asio::io_context& io_context) 
+        : acceptor_{io_context, asio::ip::tcp::endpoint{asio::ip::tcp::v4(), 44444}}
+    {
+        start_accept();
+    }
+
+private:
+    void start_accept()
+    {
+        acceptor_.async_accept([this](const system::error_code& error, asio::ip::tcp::socket socket)
+        {
+            if (!error.failed())
+            {
+                cout << "thread " << this_thread::get_id() << ": accepted\n";
+                string message{">>>> server accepted you <<<<\n"};
+                asio::async_write(socket, asio::buffer(message), 
+                                  [](const system::error_code& error, size_t)
+                {
+                    if (error.failed())
+                    {
+                        cout << error;
+                    }
+                });
+
+                start_accept();
+            }
+            else
+            {
+                cout << error;
+            }
+        });
+    }
+
+    asio::ip::tcp::acceptor acceptor_;
+};
 
 int main()
 {
     cout << "Starting main thread " << this_thread::get_id() << '\n';
 
-    io::io_service io_service;
-
-    using boost::system::error_code;
-
-    io::ip::tcp::socket socket{io_service};
-    io::ip::tcp::endpoint endpoint{io::ip::tcp::v4(), 44444};
-    io::ip::tcp::acceptor acceptor{io_service, endpoint};
-    acceptor.listen();
-
-    auto write_handler = [&socket](const error_code& error, size_t)
-    {
-        if (!error)
-        {
-            socket.shutdown(io::ip::tcp::socket::shutdown_both);
-        }
-        else
-        {
-            cout << error;
-        }
-    };
-
-    auto accept_handler = [&socket, &write_handler](const error_code& error)
-    {
-        if (!error)
-        {
-            cout << "thread " << this_thread::get_id() << ": accepted\n";
-            string message{">>>> server accepted you <<<<\n"};
-            io::async_write(socket, io::buffer(message), write_handler);
-        }
-        else
-        {
-            cout << error;
-        }
-    };
-
-    acceptor.async_accept(socket, accept_handler);
-    io_service.run();
+    asio::io_context io_context;
+    server s{io_context};
+    
+    io_context.run();
 
     return 0;
 }
