b02e65965e85e4079f2d94a8ef38be4079ca9098
twono <twono.dev@gmail.com>
2024-05-12 22:09:24 +0100

    fifo_writer fifo_reader

---
+++ ipc/fifo_reader.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+using namespace std;
+
+int main()
+{
+    const string fifo_name{"greatly_pointy"};
+    if (mknod(fifo_name.c_str(), S_IFIFO | 0666, 0) == -1)
+    {
+        if (errno != EEXIST)
+        {
+            perror("mknod");
+            return 1;
+        }
+        
+        cout << "FIFO " << fifo_name << " has been created already, fine\n";
+    }
+
+    cout << "waiting for someone to start writing from " << fifo_name << '\n';
+
+    int fifo_fd;
+    if (fifo_fd = open(fifo_name.c_str(), O_RDONLY);
+        fifo_fd == -1)
+    {
+        perror("open()");
+        return 1;
+    }
+
+    cout << "writer has arrived, start reading...\n";
+    string buf(100, '\0');
+    for (int bytes = 1; bytes > 0;)
+    {
+        if (bytes = read(fifo_fd, buf.data(), buf.size());
+            bytes == -1)
+        {
+            perror("read()");
+        }
+        else if (bytes > 0)
+        {
+            cout << "read " << bytes << " bytes: [" << string_view(buf.c_str(), bytes) << "]\n";
+        }
+    }
+
+    if (close(fifo_fd) == -1)
+    {
+        perror("close()");
+    }
+
+    return 0;
+}
---
+++ ipc/fifo_writer.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+using namespace std;
+
+int main()
+{
+    const string fifo_name{"greatly_pointy"};
+    if (mknod(fifo_name.c_str(), S_IFIFO | 0666, 0) == -1)
+    {
+        if (errno != EEXIST)
+        {
+            perror("mknod");
+            return 1;
+        }
+        
+        cout << "FIFO " << fifo_name << " has been created already, fine\n";
+    }
+
+    cout << "waiting for someone to start reading from " << fifo_name << '\n';
+
+    int fifo_fd;
+    if (fifo_fd = open(fifo_name.c_str(), O_WRONLY);
+        fifo_fd == -1)
+    {
+        perror("open()");
+        return 1;
+    }
+
+    cout << "reader has arrived, start writing...\n";
+    string buf(100, '\0');
+    int bytes_read;
+    while (true)
+    {
+        bytes_read = read(STDIN_FILENO, buf.data(), buf.size());
+        if (bytes_read == -1)
+        {
+            perror("read()");
+        }
+        if (bytes_read <= 0)
+        {
+            break;
+        }
+
+        if (int bytes_written = write(fifo_fd, buf.c_str(), bytes_read);
+            bytes_written == -1)
+        {
+            perror("write()");
+        }
+        else
+        {
+            cout << "writer wrote " << bytes_written << " bytes\n";
+        }
+    }
+
+    if (close(fifo_fd) == -1)
+    {
+        perror("close()");
+    }
+
+    return 0;
+}
