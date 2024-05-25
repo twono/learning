5fc6c5b827a3342000c265c4067a0ba1632bcea1
twono <twono.dev@gmail.com>
2024-05-25 21:30:41 +0100

    lock.cpp

---
+++ ipc/lock.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    flock fl{.l_type = F_WRLCK, .l_whence = SEEK_SET, .l_start = 0, .l_len = 0};
+    if (argc > 1) fl.l_type = F_RDLCK;
+    
+    int descriptor{-1};
+    if (descriptor = open("lock.cpp", O_RDWR); descriptor == -1)
+    {
+        perror("open()");
+        return 1;
+    }
+
+    cout << "Press <Enter> to try to get lock";
+    getchar();
+    cout << "Trying to get lock...\n";
+
+    if (fcntl(descriptor, F_SETLKW, &fl) == -1)     // blocking
+    {
+        perror("fcntl(lock)");
+        return 1;
+    }
+
+    cout << "Got lock, press <Enter> to release";
+    getchar();
+
+    fl.l_type = F_UNLCK;
+
+    if (fcntl(descriptor, F_SETLK, &fl) == -1)      // non-blocking
+    {
+        perror("fcntl(unlock)");
+        return 1;
+    }
+
+    cout << "Unlocked\n";
+    close(descriptor);
+
+    return 0;
+}
