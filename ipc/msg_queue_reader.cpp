d27eceb8cc14c417d8a66e6e0b3a45729b469ccc
twono <twono.dev@gmail.com>
2024-05-26 22:26:12 +0100

    message queues

---
+++ ipc/msg_queue_reader.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+using namespace std;
+
+struct msg_t
+{
+    long type;
+    char text[200];
+};
+
+int main()
+{
+    const int ANY_MSG_TYPE{0};
+    const int key_seed{13};
+
+    int key{-1};
+    if (key = ftok("msg_queue_writer.cpp", key_seed); key == -1)    // same as writer
+    {
+        perror("ftok");
+        return 1;
+    }
+
+    int msg_queue_id{-1};
+    if (msg_queue_id = msgget(key, 0644); msg_queue_id == -1)       // no IPC_CREAT
+    {
+        perror("msgget");
+        return 1;
+    }
+
+    cout << "Ready to receive message\n";
+
+    while (true)
+    {
+        msg_t msg;
+        if (msgrcv(msg_queue_id, &msg, sizeof(msg.text), ANY_MSG_TYPE, 0) == -1)
+        {
+            perror("msgrcv");
+            return 1;
+        }
+
+        cout << "Received: [" << msg.text << "]\n";
+    }
+
+    return 0;
+}
---
+++ ipc/msg_queue_writer.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+using namespace std;
+
+struct msg_t
+{
+    long type;
+    char text[200];
+};
+
+int main()
+{
+    const int key_seed{13};
+
+    int key{-1};
+    if (key = ftok("msg_queue_writer.cpp", key_seed); key == -1)
+    {
+        perror("ftok");
+        return 1;
+    }
+
+    int msg_queue_id{-1};
+    if (msg_queue_id = msgget(key, IPC_CREAT | 0644); msg_queue_id == -1)
+    {
+        perror("msgget");
+        return 1;
+    }
+
+    cout << "Enter text with new lines, Ctrl+D to stop:\n";
+
+    msg_t msg{.type = 1};   // doesn't matter, as long as > 0
+    while (fgets(msg.text, sizeof(msg.text), stdin) != nullptr)
+    {
+        int length = strlen(msg.text);
+        if (msg.text[length-1] == '\n') msg.text[length-1] = '\0';
+
+        if (msgsnd(msg_queue_id, &msg, length, 0) == -1)
+        {
+            perror("msgsnd");
+        }
+    }
+
+    if (msgctl(msg_queue_id, IPC_RMID, nullptr) == -1)
+    {
+        perror("msgctl(IPC_RMID)");
+        return 1;
+    }
+
+    return 0;
+}
